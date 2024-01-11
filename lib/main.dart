64f216faa0ec0b4632d134589961e2d044be100a
BetelAddisu <67442890+BetelAddisu@users.noreply.github.com>
2024-01-11 12:32:17 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:dating_app_ui_design_with_flutter/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:dating_app_ui_design_with_flutter/src/app.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const App());
+  });
+}
